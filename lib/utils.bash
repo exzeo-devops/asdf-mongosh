7813053e89bedea709529180d7556259430f0025
Lee Hull <lhull@exzeo.com>
2025-04-15 15:49:05 -0400

    changes

--- lib/utils.bash
+++ lib/utils.bash
@@ -35,7 +35,6 @@ get_architecture() {
   case $ARCH in
     x86_64) ARCH="x64";;
     arm64 | aarch64) ARCH="arm64";;
-    armv7l) ARCH="armv7l";;
     *) fail "Unsupported architecture: $ARCH";;
   esac
 
